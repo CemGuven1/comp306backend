5f31e04d773fbd78f311a53ed802c0b398e8f257
CemGuven1 <cguven20@ku.edu.tr>
2023-06-11 23:38:25 +0300

    routes update

--- routes/games.js
+++ routes/games.js
@@ -1,162 +1,95 @@
 import { Router } from 'express';
 const router = Router();
 
-//Get all games with all of its achievements
+//Get all games
 router.get('/', async (req, res) => {
   try {
-    // Retrieve all games with their achievements
+    // Retrieve all games
     const query = `
-      SELECT Games.game_id, Games.game_name, Achievements.achievement_id, Achievements.achievement_name
+      SELECT *
       FROM Games
-      LEFT JOIN Achievements ON Games.game_id = Achievements.game_id
     `;
     const [results] = await req.pool.query(query);
 
-    // Organize the data into an object with games and their achievements
-    const games = {};
-    results.forEach((row) => {
-      const { game_id, game_name, achievement_id, achievement_name } = row;
-      if (!games[game_id]) {
-        games[game_id] = {
-          game_id,
-          game_name,
-          achievements: [],
-        };
-      }
-      if (achievement_id) {
-        games[game_id].achievements.push({
-          achievement_id,
-          achievement_name,
-        });
-      }
-    });
-
-    res.json({ games });
+    res.json({ games: results });
   } catch (error) {
     console.error('Error executing MySQL query:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-//get the owned games with all of its achievements that are earned by user. (FOR INVENTORY)
-router.get('/ownedgames/User/:user_id', async (req, res) => {
-  const { user_id } = req.params;
+//get the owned games. (FOR INVENTORY)
+router.get('/User/:user_id/owned-games', async (req, res) => {
+  const user_id = req.params.user_id; // Retrieve the user ID from the route parameter
 
   try {
-    // Retrieve the games owned by the user and their earned achievements
-    const getUserGamesQuery = `
-      SELECT Games.*, Achievements.achievement_id, Achievements.achievement_name
-      FROM Games
-      INNER JOIN Inventory ON Games.game_id = Inventory.game_id
-      LEFT JOIN Achievements ON Games.game_id = Achievements.game_id
-      LEFT JOIN XP ON Achievements.achievement_id = XP.achievement_id AND XP.player_id = ?
-      WHERE Inventory.owner_id = ?
+    // Retrieve games owned by the user
+    const query = `
+      SELECT *
+      FROM Games G
+      INNER JOIN Inventory I ON G.game_id = I.game_id
+      WHERE I.owner_id = ?
     `;
-    const [gameRows] = await req.pool.query(getUserGamesQuery, [user_id, user_id]);
+    const [results] = await req.pool.query(query, [user_id]);
 
-    // Group the games by game ID and combine their earned achievements
-    const games = {};
-    gameRows.forEach((row) => {
-      const { game_id, game_name, game_price, game_description, game_category, release_date, game_rating } = row;
-
-      if (!games[game_id]) {
-        games[game_id] = {
-          game_id,
-          game_name,
-          game_price,
-          game_description,
-          game_category,
-          release_date,
-          game_rating,
-          achievements: [],
-        };
-      }
-
-      if (row.achievement_id && row.achievement_name) {
-        const isAchievementEarned = row.player_id !== null;
-        games[game_id].achievements.push({
-          achievement_id: row.achievement_id,
-          achievement_name: row.achievement_name,
-          earned: isAchievementEarned,
-        });
-      }
-    });
-
-    res.json(Object.values(games));
+    res.json({ games: results });
   } catch (error) {
     console.error('Error executing MySQL query:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-//get the unowned games by that user along with its achievements. (FOR MARKET) 
-router.get('/unownedgames/User/:user_id', async (req, res) => {
-  const { user_id } = req.params;
+//get the unowned games (FOR MARKET) 
+router.get('/User/:user_id/unowned-games', async (req, res) => {
+  const user_id = req.params.user_id; // Retrieve the user ID from the route parameter
 
   try {
-    // Retrieve the unowned games by the user and their associated achievements
-    const getUnownedGamesQuery = `
-      SELECT Games.*, Achievements.achievement_id, Achievements.achievement_name
-      FROM Games
-      LEFT JOIN Inventory ON Games.game_id = Inventory.game_id AND Inventory.owner_id = ?
-      LEFT JOIN Achievements ON Games.game_id = Achievements.game_id
-      WHERE Inventory.owner_id IS NULL
+    // Retrieve unowned games for the user
+    const query = `
+      SELECT *
+      FROM Games G
+      LEFT JOIN Inventory I ON G.game_id = I.game_id AND I.owner_id = ?
+      WHERE I.owner_id IS NULL
     `;
-    const [gameRows] = await req.pool.query(getUnownedGamesQuery, [user_id]);
+    const [results] = await req.pool.query(query, [user_id]);
 
-    // Group the games by game ID and combine their achievements
-    const games = {};
-    gameRows.forEach((row) => {
-      const { game_id, game_name, game_price, game_description, game_category, release_date, game_rating } = row;
-
-      if (!games[game_id]) {
-        games[game_id] = {
-          game_id,
-          game_name,
-          game_price,
-          game_description,
-          game_category,
-          release_date,
-          game_rating,
-          achievements: [],
-        };
-      }
-
-      if (row.achievement_id && row.achievement_name) {
-        games[game_id].achievements.push({
-          achievement_id: row.achievement_id,
-          achievement_name: row.achievement_name,
-        });
-      }
-    });
-
-    res.json(Object.values(games));
+    res.json({ games: results });
   } catch (error) {
     console.error('Error executing MySQL query:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-//get the unowned games by that user along with its achievements. (FOR MARKET RECOMMENDATION) 
-router.get('/prefferedgames/User/:user_id', async (req, res) => {
-  const { user_id } = req.params;
+//Get the unowned recommended games (FOR MARKET RECOMMENDATION) 
+router.get('/User/:user_id/recommended-games', async (req, res) => {
+  const user_id = req.params.user_id; // Retrieve the user ID from the route parameter
 
   try {
-    // Get the user's preferred game category
-    const [userCategoryRows] = await req.pool.query(
-      'SELECT game_category FROM Games JOIN Inventory ON Games.game_id = Inventory.game_id WHERE owner_id = ? GROUP BY game_category ORDER BY COUNT(*) DESC LIMIT 1',
-      [user_id]
-    );
+    // Retrieve the user's inventory preference of game category
+    const inventoryQuery = `
+      SELECT game_category, COUNT(*) AS count
+      FROM Inventory I
+      INNER JOIN Games G ON I.game_id = G.game_id
+      WHERE I.owner_id = ?
+      GROUP BY game_category
+      ORDER BY count DESC
+    `;
+    const [inventoryResults] = await req.pool.query(inventoryQuery, [user_id]);
 
-    const userPreferredCategory = userCategoryRows[0].game_category;
+    // Build the category preference order based on the user's inventory
+    const categoryOrder = inventoryResults.map((row) => row.game_category);
 
-    // Get the unowned games of the user's preferred category
-    const [unownedGamesRows] = await req.pool.query(
-      'SELECT * FROM Games WHERE game_category = ? AND game_id NOT IN (SELECT game_id FROM Inventory WHERE owner_id = ?)',
-      [userPreferredCategory, user_id]
-    );
+    // Retrieve unowned games for the user, ordered by inventory preference of game category
+    const gamesQuery = `
+      SELECT *
+      FROM Games G
+      LEFT JOIN Inventory I ON G.game_id = I.game_id AND I.owner_id = ?
+      WHERE I.owner_id IS NULL
+      ORDER BY FIELD(G.game_category, ${categoryOrder.map(() => '?').join(',')})
+    `;
+    const [gamesResults] = await req.pool.query(gamesQuery, [user_id, ...categoryOrder]);
 
-    res.json(unownedGamesRows);
+    res.json({ games: gamesResults });
   } catch (error) {
     console.error('Error executing MySQL query:', error);
     res.status(500).json({ error: 'Internal Server Error' });
