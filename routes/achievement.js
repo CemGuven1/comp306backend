5a7c11ebe6b6a2cf9872680305d75006172221f4
CemGuven1 <cguven20@ku.edu.tr>
2023-06-11 23:04:11 +0300

    route update

--- routes/achievement.js
+++ routes/achievement.js
@@ -1,24 +1,25 @@
 import { Router } from 'express';
 const router = Router();
 
-//Get spesific achievement
-router.get('/:achievement_id', async (req, res) => {
-    const achievement_id = req.params.achievement_id;
-  
-    try {
-      const query = 'SELECT * FROM Achievements WHERE achievement_id = ?';
-      const [rows] = await req.pool.query(query, [achievement_id]);
-  
-      if (rows.length === 0) {
-        res.status(404).json({ error: 'Achievement not found' });
-      } else {
-        res.json(rows[0]);
-      }
-    } catch (error) {
-      console.error('Error executing MySQL query:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+//Get spesific achievements of a game 
+router.get('/Game/:game_id', async (req, res) => {
+  const game_id = req.params.game_id; // Retrieve the game ID from the route parameter
+
+  try {
+    // Retrieve achievements specific to the game
+    const query = `
+      SELECT achievement_id, achievement_name
+      FROM Achievements
+      WHERE game_id = ?
+    `;
+    const [results] = await req.pool.query(query, [game_id]);
+
+    res.json({ achievements: results });
+  } catch (error) {
+    console.error('Error executing MySQL query:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
  //Get earned achievements of a spesific user
  router.get('/User/:user_id', async (req, res) => {
